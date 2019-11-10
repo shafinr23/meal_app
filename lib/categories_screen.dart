fae855294a30bf86e1d44c61c211a2c9558647e0
shafinr23 <shafinr23@gmail.com>
2019-11-11 02:52:27 +0600

    padding add

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,14 +5,20 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: Dummy_category.map(
-          (catData) => CategoryItem(catData.title, catData.color)).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('meal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: Dummy_category.map(
+            (catData) => CategoryItem(catData.title, catData.color)).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
