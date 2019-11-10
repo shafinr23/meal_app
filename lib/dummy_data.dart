c2c2f7e75609e8ef29df62ed15c96e9a9931d806
shafinr23 <shafinr23@gmail.com>
2019-11-10 16:15:04 +0600

    add dummy data

---
+++ lib/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import './model/category.dart';
+
+const Dummy_category = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+    Category(
+    id: 'c2',
+    title: 'bangla',
+    color: Colors.pink,
+  ),
+    Category(
+    id: 'c3',
+    title: 'indian',
+    color: Colors.blue,
+  ),
+    Category(
+    id: 'c4',
+    title: 'chines',
+    color: Colors.red,
+  ),
+    Category(
+    id: 'c4',
+    title: 'japan',
+    color: Colors.blueGrey,
+  ),
+    Category(
+    id: 'c5',
+    title: 'summer',
+    color: Colors.teal,
+  ),
+    Category(
+    id: 'c6',
+    title: 'franch',
+    color: Colors.yellowAccent,
+  ),
+    Category(
+    id: 'c7',
+    title: 'russian',
+    color: Colors.redAccent,
+  ),
+    Category(
+    id: 'c8',
+    title: 'coffee',
+    color: Colors.brown,
+  ),
+    Category(
+    id: 'c9',
+    title: 'peastry',
+    color: Colors.pinkAccent,
+  ),
+];
