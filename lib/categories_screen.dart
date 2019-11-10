8cb3bdc221e1ced3304a71a7af8eac1d346220e7
shafinr23 <shafinr23@gmail.com>
2019-11-10 16:33:13 +0600

    add items

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[
-        
-      ],
+      children: Dummy_category.map(
+          (catData) => CategoryItem(catData.title, catData.color)).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20, 
+        crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
     );
