9688d3f6d122a954867f9d699525e2c284809a32
shafinr23 <shafinr23@gmail.com>
2019-11-11 13:42:27 +0600

    add new screen in here (selectCat)

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
+import './caregory_meal_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id,this.title, this.color);
+
+  void selectCat(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealsScreen(id,title);
+        },
+        fullscreenDialog: false,
+        
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.title,
-        textAlign: TextAlign.center,
-      ),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomLeft,
+    return InkWell(
+      onTap: () => selectCat(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+          textAlign: TextAlign.center,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomLeft,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
