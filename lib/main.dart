5ae0e9f6ff02ac35b1c1037f7994352312070b53
shafinr23 <shafinr23@gmail.com>
2019-11-14 02:44:16 +0600

    fix issue

--- lib/main.dart
+++ lib/main.dart
@@ -36,9 +36,9 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName   :   (ctx)   =>  CategoryMealsScreen(),
         MealDetailScreen.routeName      :   (ctx)   =>  MealDetailScreen(),
       },
-      onGenerateRoute: (settings){
-        //return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
-      },
+      // onGenerateRoute: (settings){
+      //   //return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
+      // },
       onUnknownRoute: (settings){
         return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
       },
