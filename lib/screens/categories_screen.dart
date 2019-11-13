0a6257be8c63c952f9baec98f4f76bf403e294cd
shafinr23 <shafinr23@gmail.com>
2019-11-13 17:00:31 +0600

    remove app bar

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,23 +5,18 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('meal'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(20),
-        children: Dummy_category.map((catData) => CategoryItem(
-              catData.id,
-              catData.title,
-              catData.color,
-            )).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(20),
+      children: Dummy_category.map((catData) => CategoryItem(
+            catData.id,
+            catData.title,
+            catData.color,
+          )).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
