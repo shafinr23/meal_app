1564c78ad1a5fe935b7cc1b949d133b88257195d
shafinr23 <shafinr23@gmail.com>
2019-11-14 16:51:29 +0600

    add logic filter logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 import './screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/caregory_meal_screen.dart';
 import './screens/categories_screen.dart';
+import './model/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vagitarian': false,
+  };
+
+  List<Meal> _avaiableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+      _avaiableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters['vagitarian'] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,24 +63,25 @@ class MyApp extends StatelessWidget {
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
             title: TextStyle(
-              fontSize: 20, 
+              fontSize: 20,
               fontFamily: 'RobotoCondensed',
-            )
-            ),
+            )),
       ),
       //home: CategoriesScreen(),
-      initialRoute: '/', //defalt 
+      initialRoute: '/', //defalt
       routes: {
-        '/'                             :   (ctx)   => TabsScreen(),
-        CategoryMealsScreen.routeName   :   (ctx)   =>  CategoryMealsScreen(),
-        MealDetailScreen.routeName      :   (ctx)   =>  MealDetailScreen(),
-        FilterScreen.routeName          :   (ctx)   => FilterScreen(),
+        '/': (ctx) => TabsScreen(),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_avaiableMeals),
+        MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FilterScreen.routeName: (ctx) => FilterScreen(_filters,_setFilters),
       },
-      // onGenerateRoute: (settings){
-      //   //return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
-      // },
-      onUnknownRoute: (settings){
-        return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
+      onGenerateRoute: (settings) {
+        print(settings.arguments);
+        //return MaterialPageRoute(builder: (ctx)=>CategoriesScreen());
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
     );
   }
