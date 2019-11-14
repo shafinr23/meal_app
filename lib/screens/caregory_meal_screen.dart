ae44fea17a010d34f2b3c8b89dfc1897a3a1c54a
shafinr23 <shafinr23@gmail.com>
2019-11-14 16:50:52 +0600

    add available meal

--- lib/screens/caregory_meal_screen.dart
+++ lib/screens/caregory_meal_screen.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import '../widgets/meal_item.dart';
 import '../model/meal.dart';
 
-import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
+  final List<Meal> availableMeals; 
 
+  CategoryMealsScreen(this.availableMeals);
   @override
   _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
 }
@@ -30,7 +31,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
       categoryTitle = routeArg['title'];
       final categoryId = routeArg['id'];
 //for binding
-      displayedMeals = DUMMY_MEALS.where((meal) {
+      displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories.contains(categoryId);
       }).toList();
       _loadInitData = true;
